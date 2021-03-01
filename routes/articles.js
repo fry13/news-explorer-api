c14fcb0a5596d75a7b71e04ee98f0dfb0a3d3d52
Alexey Feklyunin <62357216+fry13@users.noreply.github.com>
2021-03-01 07:05:32 +0400

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -15,8 +15,8 @@ router.post('/', celebrate({
     text: Joi.string().required(),
     date: Joi.string().required(),
     source: Joi.string().required(),
-    link: Joi.string().regex(/(^https?:\/\/)?[a-z0-9~_\-.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)?$/i).required(),
-    image: Joi.string().regex(/(^https?:\/\/)?[a-z0-9~_\-.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)?$/i).required(),
+    link: Joi.string().required(),
+    image: Joi.string().required(),
     owner: Joi.string().hex().length(24),
   }),
 }), auth, postArticle);
